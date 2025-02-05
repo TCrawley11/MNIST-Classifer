eda674e550d0ba020654d421d18af25ab1a4416d
Tygo <tcrawley@uwo.ca>
2025-02-05 04:00:59 -0500

    Fixing weird language extension

--- MNISTenv/main.py
+++ MNISTenv/main.py
@@ -87,15 +87,15 @@ def main():
     parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
     # For visualizing on tensorboard
     writer = SummaryWriter()
-    
+
     parser.add_argument('--batch-size', type=int, default=64, metavar='N',
                         help='input batch size for training (default: 64)')
     parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
                         help='input batch size for testing (default: 1000)')
     parser.add_argument('--epochs', type=int, default=14, metavar='N',
                         help='number of epochs to train (default: 14)')
-    parser.add_argument('--lr', type=float, default=1.0, metavar='LR',
-                        help='learning rate (default: 1.0)')
+    parser.add_argument('--lr', type=float, default=0.0001, metavar='LR',
+                        help='learning rate (default: 0.0001)')
     parser.add_argument('--gamma', type=float, default=0.7, metavar='M',
                         help='Learning rate step gamma (default: 0.7)')
     parser.add_argument('--no-cuda', action='store_true', default=False,
